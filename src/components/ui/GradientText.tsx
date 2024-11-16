5deb74f4eee7ec7953ddabc069393d528090c475
MisterMunchkin <9417970+MisterMunchkin@users.noreply.github.com>
2024-11-16 22:15:36 +1000

    added gradient text component

---
+++ src/components/ui/GradientText.tsx
@@ -0,0 +1,17 @@
+import "@css/animation-gradient-text.css";
+import { cn } from "@utilities/cn";
+import type { ClassValue } from "clsx";
+import type { ReactNode } from "react";
+
+type Props = {
+    children: string;
+    className?: ClassValue;
+};
+
+const GradientText = ({ className, children }: Props) => {
+    return (
+        <span className={cn("gradient-anim-right", className)}>{children}</span>
+    );
+};
+
+export default GradientText;
