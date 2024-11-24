4d2a1da1cf8390348eeaef4ce0a86eda5457a250
MisterMunchkin <9417970+MisterMunchkin@users.noreply.github.com>
2024-11-24 18:03:25 +1000

    added content collection

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z, type ImageFunction } from "astro:content";
+
+const workExperienceSchema = (image: ImageFunction) =>
+    z.object({
+        title: z.string(),
+        company: z.string(),
+        timeline: z.string(),
+        website: z.string(),
+        points: z.array(z.string()),
+    });
+
+const experienceCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) => workExperienceSchema(image),
+});
+
+export const collections = {
+    "work-experiences": experienceCollection,
+};
+
+export type WorkExperience = z.infer<ReturnType<typeof workExperienceSchema>>;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,10 @@ const { nextui } = require("@nextui-org/theme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         fontFamily: {
             sans: ["Geist", defaultTheme.fontFamily.sans],
