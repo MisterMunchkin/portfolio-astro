bedf0ee6fa21e0c97fe87a161c492764cab6d3a6
MisterMunchkin <9417970+MisterMunchkin@users.noreply.github.com>
2024-12-01 22:39:45 +1000

    added better keys

--- src/components/sections/whoami/Skills.tsx
+++ src/components/sections/whoami/Skills.tsx
@@ -6,19 +6,16 @@ type Props = {};
 const Skills = ({}: Props) => {
     return (
         <div className="flex flex-row justify-center items-start gap-20 gap-y-10 flex-wrap">
-            {Object.entries(CONTENT.SKILLS).map(([key, skills], index) => (
-                <div className="flex flex-col">
-                    <h4
-                        className="font-semibold text-amaranth-500 mb-2"
-                        key={index}
-                    >
+            {Object.entries(CONTENT.SKILLS).map(([key, skills], skillIndex) => (
+                <div key={skillIndex} className="flex flex-col">
+                    <h4 className="font-semibold text-amaranth-500 mb-2">
                         {key}
                     </h4>
                     <div className="flex flex-col gap-1">
                         {skills.map((skill, index) => (
                             <>
                                 <span
-                                    key={index}
+                                    key={`${skillIndex}_${index}`}
                                     className="font-medium text-star-dust-600"
                                 >
                                     {skill.text} {skill.content}
